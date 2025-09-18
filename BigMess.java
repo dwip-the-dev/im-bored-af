390e9f771197c520b6535558f70ee88654cf8df5
Dwip Biswas <dwipbiswas@yahoo.com>
2025-09-18 16:24:31 +0530

    trying java after over 6months of not touching it

---
+++ BigMess.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+class MegaClass1 {
+    void useless1() {
+        int x = new Random().nextInt(100);
+        // does literally nothing
+    }
+    void useless2() {
+        for (int i = 0; i < 10; i++) useless1();
+    }
+}
+
+class MegaClass2 {
+    void useless3() {
+        MegaClass1 mc = new MegaClass1();
+        mc.useless2();
+    }
+}
+
+class MegaClass3 {
+    void uselessChain() {
+        MegaClass2 mc2 = new MegaClass2();
+        for (int i = 0; i < 100; i++) mc2.useless3();
+    }
+}
+
+public class BigMess {
+    public static void main(String[] args) {
+        MegaClass3 mc3 = new MegaClass3();
+        mc3.uselessChain();
+        System.out.println("Hello… I do almost nothing 💀");
+    }
+}
